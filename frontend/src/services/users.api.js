7b4460f3a7ca72263e75a3139bdf63e97f409717
Deepakcodes30 <deepak.chouhan9892@gmail.com>
2026-01-27 18:23:01 +0530

    feat: implement user services for registration, login, logout, password change, and account updates

---
+++ frontend/src/services/users.api.js
@@ -0,0 +1,80 @@
+import api from "./api";
+import { fetchWrapper } from "./fetchWrapper";
+
+async function registerUser({ fullName, phoneNumber, password, email }) {
+  return fetchWrapper(async () => {
+    const res = await api.post("/users/register-user", {
+      fullName,
+      phoneNumber,
+      password,
+      email,
+    });
+    return res.data;
+  });
+}
+
+async function loginUser({ email, phoneNumber, password }) {
+  return fetchWrapper(async () => {
+    const res = await api.post("/users/login", {
+      email,
+      phoneNumber,
+      password,
+    });
+    return res.data;
+  });
+}
+
+async function logoutUser() {
+  return fetchWrapper(async () => {
+    const res = await api.post("/users/logout");
+    return res.data;
+  });
+}
+
+async function changeCurrentPassword({ oldPassword, newPassword }) {
+  if (!oldPassword) throw new Error("Old password is required");
+  if (!newPassword) throw new Error("New password is required");
+
+  return fetchWrapper(async () => {
+    const res = await api.post("/users/change-current-password", {
+      oldPassword,
+      newPassword,
+    });
+    return res.data;
+  });
+}
+
+async function getCurrentUser() {
+  return fetchWrapper(async () => {
+    const res = await api.get("/users/get-current-user");
+    return res.data;
+  });
+}
+
+async function updateAccountDetails({ phoneNumber, email, fullName }) {
+  return fetchWrapper(async () => {
+    const res = await api.patch("/users/update-account-details", {
+      phoneNumber,
+      email,
+      fullName,
+    });
+    return res.data;
+  });
+}
+
+async function updateUserAvatar(formData) {
+  return fetchWrapper(async () => {
+    const res = await api.patch("/users/update-user-avatar", formData);
+    return res.data;
+  });
+}
+
+export const userServices = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+};
